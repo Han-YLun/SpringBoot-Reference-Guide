89f1e09b5a86091dbc7ffc9dacc40e511beeaef5
Han-YLun <ArvinHan@foxmail.com>
2019-11-12 20:47:43 +0800

    update the prime-java plugin

--- docs/js/prism-java.js
+++ docs/js/prism-java.js
@@ -1,6 +1,7 @@
 (function (Prism) {
 
-	var keywords = new RegExp('/\b(?:abstract|continue|for|new|switch|assert|default|goto
+	var keywords = new RegExp('
+		/\b(?:abstract|continue|for|new|switch|assert|default|goto
 	|package|synchronized|boolean|do|if|private|this|break|double|implements
 	|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return
 	|transient|catch|extends|int|short|try|char|final|interface|static|void|class
